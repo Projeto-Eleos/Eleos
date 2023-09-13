44d9b9437c5f27374197a446a95417b5c8df5ed6
Guilherme <guilherme.frias.o.t3@gmail.com>
2023-09-13 20:50:34 -0300

    [Fix]: Solving all form action methods and rename some files

--- src/main/java/com/example/javawebapp/AuthPasswordChangeServlet.java
+++ src/main/java/com/example/javawebapp/AuthPasswordChangeServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "authpasswordchange", value = "/auth-password-change")
+@WebServlet(name = "authPasswordChange", value = "/authPasswordChange")
 public class AuthPasswordChangeServlet extends HttpServlet {
 
     @Override
@@ -25,16 +25,16 @@ public class AuthPasswordChangeServlet extends HttpServlet {
         // long cod_digitado = Long.parseLong(request.getParameter("cod"));
 
         // if (cod_gerado == cod_digitado) {
-        //     request.setAttribute("codUser", "<id do usuário>");
+        // request.setAttribute("codUser", "<id do usuário>");
 
-        //     // Obtém o RequestDispatcher
-        //     RequestDispatcher dispatcher = request.getRequestDispatcher("/Servlet2");
+        // // Obtém o RequestDispatcher
+        // RequestDispatcher dispatcher = request.getRequestDispatcher("/Servlet2");
 
-        //     // Encaminha a solicitação para Servlet2
-        //     dispatcher.forward(request, response);
-        //     response.sendRedirect("/change-password.html");
+        // // Encaminha a solicitação para Servlet2
+        // dispatcher.forward(request, response);
+        // response.sendRedirect("/change-password.html");
         // } else {
-        //     response.sendRedirect("/código-inválido.html");
+        // response.sendRedirect("/código-inválido.html");
         // }
 
         System.out.println(email);
--- src/main/java/com/example/javawebapp/SignInServlet.java
+++ src/main/java/com/example/javawebapp/SignInServlet.java
@@ -8,11 +8,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "login", value = "/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet(name = "signIn", value = "/signIn")
+public class SignInServlet extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String email = req.getParameter("email");
-        String password = req.getParameter("password")
+        String password = req.getParameter("password");
         System.out.println(email);
         System.out.println(password);
     }
